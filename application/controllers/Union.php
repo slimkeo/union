1506954fca141abe51deeb986ba4b7e7da083e4b
Thabani Sicelo Hlanze <sicelohlanze@gmail.com>
2026-04-22 08:46:14 +0200

    UI

--- application/controllers/Union.php
+++ application/controllers/Union.php
@@ -2231,7 +2231,7 @@ public function member_subscription($memberid)
                 // Send SMS only if not already sent and if cellnumber exists
                 if (!$sms_exists && !empty($cellnumber)) {
                     // Construct SMS message
-                    $sms_message = "Valued Member, your Number is 058-{$member_id}. Tell other VMs to update their KYC for Union Numbers here https://tinyurl.com/594xz6kk";
+                    $sms_message = "Valued Member, your Number is 058-{$member_id}. Tell other VMs to update their KYC for Union Numbers here https://tinyurl.com/594xz6kk I";
                     // Send SMS
                     $sms_result = $this->broadcast_message($cellnumber, $sms_message);
                     
