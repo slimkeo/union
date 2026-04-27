193f4347026a330259eeaf0129cfc8cf6bae923a
Thabani Sicelo Hlanze <sicelohlanze@gmail.com>
2026-04-27 16:53:40 +0200

    snatunion

--- application/controllers/Union.php
+++ application/controllers/Union.php
@@ -1380,7 +1380,7 @@ public function member_subscription($memberid)
 
         $page_data['event_id'] = $this->input->post('event_id');   
         $page_data['page_name']  = 'report_per_event';
-        $page_data['page_title'] = $this->db->get_where('events', array('id' => $page_data['event_id']))->row()->description.' Details';
+        $page_data['page_title'] = ' Details';
         $this->load->view('backend/index', $page_data);
     }       
     /********** MANAGE USERS (System Users / Admins) ********************/
