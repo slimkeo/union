f225ba1f1672b1fc509b390eeb236bb15b1feeb2
Thabani Sicelo Hlanze <sicelohlanze@gmail.com>
2026-05-26 23:42:37 +0200

    print all

--- application/views/backend/union/report_per_branch.php
+++ application/views/backend/union/report_per_branch.php
@@ -40,7 +40,7 @@ $branch_name = $branch ? $branch['name'] : 'Unknown Branch';
             return;
         }
 
-        jQuery('#datatable-branch-members').DataTable({
+        var table = jQuery('#datatable-branch-members').DataTable({
             processing: true,
             serverSide: true,
             ajax: {
@@ -57,7 +57,35 @@ $branch_name = $branch ? $branch['name'] : 'Unknown Branch';
                 { extend: 'copy', text: 'Copy' },
                 { extend: 'excel', text: 'Excel' },
                 { extend: 'pdf', text: 'PDF' },
-                { extend: 'print', text: 'Print' }
+                {
+                    extend: 'print',
+                    text: 'Print',
+                    action: function (e, dt, button, config) {
+                        var oldLength = dt.page.len();
+                        var oldPage = dt.page();
+
+                        // For server-side tables, only current page is loaded.
+                        // Temporarily load all rows, print, then restore paging.
+                        dt.one('draw', function () {
+                            if (jQuery.fn.dataTable.ext.buttons.print && jQuery.fn.dataTable.ext.buttons.print.action) {
+                                jQuery.fn.dataTable.ext.buttons.print.action.call(this, e, dt, button, config);
+                            } else {
+                                window.print();
+                            }
+
+                            // Restore after print dialog is opened
+                            setTimeout(function () {
+                                dt.page.len(oldLength);
+                                dt.one('draw', function () {
+                                    dt.page(oldPage).draw('page');
+                                });
+                                dt.draw(false);
+                            }, 500);
+                        });
+
+                        dt.page.len(-1).draw();
+                    }
+                }
             ]
         });
     });
