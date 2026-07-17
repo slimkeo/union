666faac11a8612c71b4d18960241ee0971351316
Thabani Sicelo Hlanze <sicelohlanze@gmail.com>
2026-07-17 10:04:49 +0200

    all set

--- application/models/Member_model.php
+++ application/models/Member_model.php
@@ -10,18 +10,18 @@ class Member_model extends CI_Model {
 
     public function count_all_members()
     {
-        //return (int)$this->db->count_all('members'); 
+        return (int)$this->db->count_all('members'); 
 
         //testing
-        $this->db->where_in('id', range(1, 20));
-       return (int)$this->db->count_all_results('members');
+        //$this->db->where_in('id', range(1, 20));
+       //return (int)$this->db->count_all_results('members');
     }
 
     public function get_members_batch($offset = 0, $limit = 200)
     {
         $this->db->select('id, idnumber, employeeno, tscno, surname, name, cellnumber, dob, gender, schoolcode');
         $this->db->from('members');
-        $this->db->where_in('id', range(1, 20));
+        //$this->db->where_in('id', range(1, 20)); // testing
         $this->db->limit($limit, $offset);
         $query = $this->db->get();
         return $query->result_array();
